1b67fc65e79555caecde0ae812c7de2c2d65806a
badcakee <antta6487@gmail.com>
2025-07-16 22:50:46 +0200

    Update main.sh

--- badcakee/main.sh
+++ badcakee/main.sh
@@ -1,5 +1,7 @@
 echo Installing useful dependencies :D
+add-apt-repository ppa:apt-fast/stable -y
 apt update
-apt install curl wget nano git unzip zip tar htop net-tools neofetch -y
-apt upgrade -y
+apt install apt-fast -y
+apt-fast curl wget nano git unzip zip tar htop net-tools neofetch -y
+apt-fast upgrade -y
 echo ✅ Installed curl, wget, nano, git, unzip, zip, tar, htop, net-tools and neofetch
