2b2e55402671808f1b94778124d5402364dac439
badcakee <antta6487@gmail.com>
2025-07-16 22:49:06 +0200

    Create main.sh

---
+++ badcakee/main.sh
@@ -0,0 +1,5 @@
+echo Installing useful dependencies :D
+apt update
+apt install curl wget nano git unzip zip tar htop net-tools neofetch -y
+apt upgrade -y
+echo ✅ Installed curl, wget, nano, git, unzip, zip, tar, htop, net-tools and neofetch
